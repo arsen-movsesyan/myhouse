cf81b5b224f597e3405b9da01afab32173e167d8
movsesyan1970 <arsen.movsesyan@gmail.com>
2015-08-17 18:10:10 -0700

    Created new directory structure

--- myhouse/settings.py
+++ myhouse/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+#    'manager',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -50,7 +51,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.security.SecurityMiddleware',
 )
 
-ROOT_URLCONF = 'household.urls'
+ROOT_URLCONF = 'myhouse.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'household.wsgi.application'
+WSGI_APPLICATION = 'myhouse.wsgi.application'
 
 
 # Database
